7d74af0da82559e5e65c9b03992f5e329a450d67
shuja-shah <shujashah786@gmail.com>
2024-05-17 00:22:54 +0500

    Saved Detection Info

--- handlers/modules/run_yolo.py
+++ handlers/modules/run_yolo.py
@@ -15,6 +15,22 @@ def run_yolo_inference(input_image, index, mission_title="mission"):
     model_path = os.path.join(os.path.dirname(__file__), "model", "md.pt")
     model = YOLO(model_path)
     result = model(image_rgb, conf=0.5)
+
+    # detections_info = []
+    # for detection in result[0].predictions:
+    #     label = detection.label
+    #     confidence = detection.confidence
+    #     bounding_box = detection.box
+    #     detections_info.append(
+    #         {"label": label, "confidence": confidence, "bounding_box": bounding_box}
+    #     )
+
+    detections_info = {
+        "detections": result[0].verbose(),  
+        "speed": result[0].speed,           
+        "names": result[0].names,
+          
+    }
     detections = result[0].plot()
     result_dir = os.path.join(
         os.path.dirname(os.path.abspath(sys.argv[0])), "results", mission_title
